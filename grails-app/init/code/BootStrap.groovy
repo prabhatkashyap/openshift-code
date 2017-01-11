f0e3d9590065fb6fdb8359e71b9b63e69070d2d1
Prabhat Kashyap <prabhat.kashyap.28@gmail.com>
2017-01-12 00:41:49 +0530

    ..

--- grails-app/init/code/BootStrap.groovy
+++ grails-app/init/code/BootStrap.groovy
@@ -3,6 +3,8 @@ package code
 class BootStrap {
 
     def init = { servletContext ->
+        println("///////Started Bootstrap//////")
+        println("///////End Bootstrap//////")
     }
     def destroy = {
     }
