cfd9814c4039ea6be1dde802d0f38f5db8ec1713
Prabhat Kashyap <prabhat.kashyap.28@gmail.com>
2017-01-16 23:37:15 +0530

    ..

--- grails-app/controllers/code/UrlMappings.groovy
+++ grails-app/controllers/code/UrlMappings.groovy
@@ -9,7 +9,7 @@ class UrlMappings {
             }
         }
 
-        "/"(view: "/template/index")
+        "/"(view: "/index")
         "500"(view: '/error')
         "404"(view: '/notFound')
     }
